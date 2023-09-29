c035a69f240ed08131fc2faab7e80fddb020802c
hjeong6219 <it.josh.jeong@gmail.com>
2023-09-28 23:49:16 -0400

    connected frontend with backend

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const PORT = 5050;
 
+app.use(cors());
+
 app.get("/api/home", (req, res) => {
-  res.json({ message: "Hello World!" });
+  res.json({ message: "Hello World!", people: ["Harry", "Jack", "Barry"] });
 });
 
 app.listen(PORT, () => {
--- frontend/src/app/[[...slug]]/page.jsx
+++ frontend/src/app/[[...slug]]/page.jsx
@@ -1,10 +1,33 @@
 "use client";
 
-import dynamic from "next/dynamic";
-import "../globals.css";
+// import dynamic from "next/dynamic";
+// import "../globals.css";
 
-const App = dynamic(() => import("../../App"), { ssr: false });
+// const App = dynamic(() => import("../../App"), { ssr: false });
 
-export default function Page() {
-  return <App />;
+// export default function Page() {
+//   return <App />;
+// }
+
+import React, { useEffect, useState } from "react";
+
+function page() {
+  const [message, setMessage] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5050/api/home")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setMessage(data.people);
+      });
+  }, []);
+  return (
+    <div>
+      {message.map((j, k) => (
+        <div key={k}>{j}</div>
+      ))}
+    </div>
+  );
 }
+
+export default page;
