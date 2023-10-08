79a98ea2dcdedb0f39b7c66e08c21160fd036b3e
hjeong6219 <it.josh.jeong@gmail.com>
2023-10-08 16:40:16 -0400

    commented out codes already handled by kinde and made minor modifications to the other api functions

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
+// const bcrypt = require("bcrypt");
 const Users = require("../../models/Users");
 const router = express.Router();
 
@@ -20,7 +20,22 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.delete("/delete/:email", async (req, res) => {
+router.get("/", async (req, res) => {
+  try {
+    const { email } = req.query;
+    const userExists = await Users.findOne({ email });
+    if (!userExists) {
+      console.log(`User with email ${email} does not exist.`);
+      return res.status(401).json({ message: "User does not exist." });
+    }
+    res.json(userExists);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+router.delete("/:email", async (req, res) => {
   try {
     const { email } = req.body;
     const userExists = await Users.findOne({ email });
@@ -36,91 +51,60 @@ router.delete("/delete/:email", async (req, res) => {
   }
 });
 
-router.post("/email-reset", async (req, res) => {
-  try {
-    const { email } = req.body;
-    const userExists = await Users.findOne({ email });
+// below code are handled by kinde but keeping for reference
+// router.post("/email-reset", async (req, res) => {
+//   try {
+//     const { email } = req.body;
+//     const userExists = await Users.findOne({ email });
 
-    if (!userExists) {
-      return res.status(401).json({ message: "User does not exist." });
-    }
-    res.json({ message: "Confirmation email has been sent" });
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Server Error");
-  }
-});
+//     if (!userExists) {
+//       return res.status(401).json({ message: "User does not exist." });
+//     }
+//     res.json({ message: "Confirmation email has been sent" });
+//   } catch (err) {
+//     console.error(err.message);
+//     res.status(500).send("Server Error");
+//   }
+// });
 
-router.post("/password-reset", async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    const userExists = await Users.findOne({ email });
+// router.post("/password-reset", async (req, res) => {
+//   try {
+//     const { email, password } = req.body;
+//     const userExists = await Users.findOne({ email });
 
-    if (!userExists) {
-      return res.status(401).json({ message: "User does not exist." });
-    }
+//     if (!userExists) {
+//       return res.status(401).json({ message: "User does not exist." });
+//     }
 
-    // Password complexity validation
-    const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
-    if (!passwordRegex.test(password)) {
-      return res
-        .status(401)
-        .json({ message: "Password does not meet complexity requirements." });
-    }
+//     // Password complexity validation
+//     const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+//     if (!passwordRegex.test(password)) {
+//       return res
+//         .status(401)
+//         .json({ message: "Password does not meet complexity requirements." });
+//     }
 
-    const saltRounds = 10;
-    bcrypt.hash(password, saltRounds, async (err, hash) => {
-      if (err) {
-        res
-          .status(500)
-          .json({ msg: "An error occurred while hashing the password." });
-      }
-      const isMatch = await bcrypt.compare(password, userExists.password);
-      if (isMatch) {
-        return res.status(401).json({
-          message: "New password cannot be the same as old password.",
-        });
-      }
-      userExists.password = hash;
-      await userExists.save();
-      res.json({ message: "Password reset successfully." });
-    });
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Server Error");
-  }
-});
-
-router.get("/", async (req, res) => {
-  try {
-    const { email } = req.query;
-    const userExists = await Users.findOne({ email });
-    if (!userExists) {
-      console.log(`User with email ${email} does not exist.`);
-      return res.status(401).json({ message: "User does not exist." });
-    }
-    res.json(userExists);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Server Error");
-  }
-});
-router.post("/login", async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    const userExists = await Users.findOne({ email });
-    if (!userExists) {
-      return res.status(401).json({ message: "User does not exist." });
-    }
-    const checkPassword = await bcrypt.compare(password, userExists.password);
-    if (!checkPassword) {
-      return res.status(401).json({ message: "Incorrect password." });
-    }
-    res.json(userExists);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Server Error");
-  }
-});
+//     const saltRounds = 10;
+//     bcrypt.hash(password, saltRounds, async (err, hash) => {
+//       if (err) {
+//         res
+//           .status(500)
+//           .json({ msg: "An error occurred while hashing the password." });
+//       }
+//       const isMatch = await bcrypt.compare(password, userExists.password);
+//       if (isMatch) {
+//         return res.status(401).json({
+//           message: "New password cannot be the same as old password.",
+//         });
+//       }
+//       userExists.password = hash;
+//       await userExists.save();
+//       res.json({ message: "Password reset successfully." });
+//     });
+//   } catch (err) {
+//     console.error(err.message);
+//     res.status(500).send("Server Error");
+//   }
+// });
 
 module.exports = router;
