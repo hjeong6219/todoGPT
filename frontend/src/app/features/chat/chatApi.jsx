97de4327c2ee00e884763bb6b33cd8ba1b998456
hjeong6219 <it.josh.jeong@gmail.com>
2023-10-09 22:46:04 -0400

    fixed chat api path

--- frontend/src/app/features/chat/chatApi.jsx
+++ frontend/src/app/features/chat/chatApi.jsx
@@ -1,29 +1,29 @@
-import { createApi } from "@reduxjs/toolkit/query";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const chatApi = createApi({
   reducerPath: "chatApi",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/" }),
   endpoints: (builder) => ({
     getChatByTodo: builder.query({
-      query: (todoId) => `chat?todoId=${todoId}`,
+      query: (todoId) => `chats?todoId=${todoId}`,
     }),
     addChat: builder.mutation({
       query: (data) => ({
-        url: "chat",
+        url: "chats",
         method: "POST",
         body: data,
       }),
     }),
     updateChat: builder.mutation({
       query: ({ _id, ...data }) => ({
-        url: `chat/${_id}`,
+        url: `chats/${_id}`,
         method: "PUT",
         body: data,
       }),
     }),
     deleteChat: builder.mutation({
       query: (_id) => ({
-        url: `chat/${_id}`,
+        url: `chats/${_id}`,
         method: "DELETE",
       }),
     }),
