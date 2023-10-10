322e8e6a2ff29f1d23bf598fe99e16a0e46683f3
hjeong6219 <it.josh.jeong@gmail.com>
2023-10-09 22:46:32 -0400

    created message component and tested

--- frontend/src/app/chat/page.jsx
+++ frontend/src/app/chat/page.jsx
@@ -1,11 +1,12 @@
 import ChatInput from "@/components/chat/ChatInput";
+import Message from "@/components/chat/Message";
 
 function Page() {
   return (
     <div className="absolute inset-0 z-50 items-center justify-center w-2/5 max-w-screen-xl m-auto overflow-x-hidden overflow-y-auto shadow-lg h-3/5 rounded-xl bg-stone-50 border-stone-400 focus:outline-none">
       <div className="flex h-full ">
         <section className="relative flex items-stretch flex-1 h-full py-2 text-2xl resize-none scrollbar-hide focus:outline-none bg-stone-50 text-stone-900">
-          Hi
+          <Message />
         </section>
         <ChatInput />
       </div>
--- frontend/src/components/chat/Message.jsx
+++ frontend/src/components/chat/Message.jsx
@@ -1,5 +1,33 @@
+"use client";
+import { useGetChatByTodoQuery } from "@/app/features/chat/chatApi";
+
 function Message() {
-  return <div></div>;
+  const {
+    data: chat,
+    isLoading,
+    isError,
+  } = useGetChatByTodoQuery("65237f11c6758fe1b118185c");
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  console.log(chat);
+
+  if (isError) {
+    return <div>Error fetching chat data</div>;
+  }
+
+  return (
+    <div>
+      {chat &&
+        chat[0].messages.map((message) => (
+          <div key={message._id}>
+            <p>{message.content}</p>
+            <p>{message.sender}</p>
+          </div>
+        ))}
+    </div>
+  );
 }
 
 export default Message;
