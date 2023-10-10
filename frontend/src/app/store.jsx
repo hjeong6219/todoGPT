988bd38b741035df328492c62d40452fdcf20fd0
hjeong6219 <it.josh.jeong@gmail.com>
2023-10-09 22:22:50 -0400

    added chat api to the store

--- frontend/src/app/store.jsx
+++ frontend/src/app/store.jsx
@@ -2,14 +2,20 @@ import { configureStore } from "@reduxjs/toolkit";
 import { todosApi } from "./features/todo/todosApi";
 import { usersApi } from "./features/todo/usersApi";
 import { setupListeners } from "@reduxjs/toolkit/query";
+import { chatApi } from "./features/chat/chatApi";
 
 const store = configureStore({
   reducer: {
     [todosApi.reducerPath]: todosApi.reducer,
     [usersApi.reducerPath]: usersApi.reducer,
+    [chatApi.reducerPath]: chatApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(todosApi.middleware, usersApi.middleware),
+    getDefaultMiddleware().concat(
+      todosApi.middleware,
+      usersApi.middleware,
+      chatApi.middleware
+    ),
 });
 
 setupListeners(store.dispatch);
