a51c94d29cf8bee9c0c37b461e1fe6ca003ecb9b
hjeong6219 <it.josh.jeong@gmail.com>
2023-10-11 10:48:35 -0400

    added function to show line breaks in the messages

--- frontend/src/components/chat/ChatBubble.jsx
+++ frontend/src/components/chat/ChatBubble.jsx
@@ -1,4 +1,7 @@
 function ChatBubble({ sender, content }) {
+  // Needed to display the messages with line breaks
+  const formattedContent = content.replace(/\n/g, "<br>");
+
   return (
     <div
       className={`chat my-8 ${sender === "user" ? "chat-end" : "chat-start"}`}
@@ -7,9 +10,8 @@ function ChatBubble({ sender, content }) {
         className={`chat-bubble ${
           sender === "user" ? "chat-bubble-accent" : "chat-bubble-info"
         }`}
-      >
-        {content}
-      </div>
+        dangerouslySetInnerHTML={{ __html: formattedContent }}
+      />
     </div>
   );
 }
