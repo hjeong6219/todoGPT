9aa6b6110e191f597c2758fa065c52a4b2a79ea7
hjeong6219 <it.josh.jeong@gmail.com>
2023-10-11 09:31:25 -0400

    cleaned up the deleted route

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,6 @@ const app = express();
 const todos = require("./routes/api/todos");
 const users = require("./routes/api/users");
 const chats = require("./routes/api/chats");
-const openai = require("./routes/api/openAI");
 const cors = require("cors");
 
 connectDB();
@@ -17,7 +16,6 @@ app.use(express.json());
 app.use("/todos", todos);
 app.use("/users", users);
 app.use("/chats", chats);
-app.use("/openai", openai);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
