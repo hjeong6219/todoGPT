9331b6865d34dd00d79e51c6c1c88b58bfaa0e27
hjeong6219 <it.josh.jeong@gmail.com>
2023-10-15 02:27:36 -0400

    refactored components to be more concise and modular to be used in other pages if necessary

---
+++ frontend/src/components/todo/TodoWrapper.jsx
@@ -0,0 +1,66 @@
+import {
+  useGetTodosByIdQuery,
+  useUpdateTodoMutation,
+} from "@/app/features/todo/todosApi";
+import Chat from "../chat/Chat";
+import EditorButton from "./EditorButton";
+import Title from "./Title";
+import TodoContent from "./TodoContent";
+
+function TodoWrapper({ todo, setShowTodo }) {
+  const { data: todoData, isLoading: isLoadingTodo } = useGetTodosByIdQuery(
+    todo?._id,
+    { skip: !todo }
+  );
+  const [updateTodo, { data: updateTodoData, error: updateTodoError }] =
+    useUpdateTodoMutation();
+
+  const handleUpdate = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const values = {
+      title: formData.get("title"),
+      content: formData.get("content"),
+    };
+    console.log(values);
+    try {
+      await updateTodo({
+        ...todoData,
+        title: formData.get("title"),
+        content: formData.get("content"),
+      });
+    } catch (error) {
+      console.error("Failed to update todo");
+    }
+  };
+
+  function handleClose() {
+    setShowTodo(false);
+  }
+
+  if (isLoadingTodo) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <section className="absolute left-0 right-0 z-30 items-center justify-center w-4/5 p-4 mx-auto my-2 shadow-lg max-w-screen-2xl border-stone-400 rounded-xl bg-stone-100 h-3/5 ">
+      {todoData && (
+        <form className="relative w-full h-full" onSubmit={handleUpdate}>
+          <div className="sticky w-full">
+            <div className="flex items-center justify-center bg-stone-200 rounded-xl">
+              <Title todoTitle={todoData.title} />
+              <EditorButton type="submit" />
+              <EditorButton type="close" onClick={handleClose} />
+            </div>
+          </div>
+          <div className="relative flex items-stretch h-full pt-2 pb-12">
+            <TodoContent todoContent={todoData.content} />
+            <Chat todoId={todoData._id} />
+          </div>
+        </form>
+      )}
+    </section>
+  );
+}
+
+export default TodoWrapper;
