3cb46a711f077655616782718423db5f662ecda5
hjeong6219 <it.josh.jeong@gmail.com>
2023-10-23 01:07:48 -0400

    fixed classname syntax

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -3,7 +3,7 @@ import { ImSpinner2 } from "react-icons/im";
 
 function Loader() {
   return (
-    <button class="shadow-lg rounded-md flex p-4" disabled>
+    <button className="shadow-lg rounded-md flex p-4" disabled>
       <ImSpinner2 className="animate-spin mr-4 text-4xl" />
       <p className=" text-4xl">Loading...</p>
     </button>
