365363295678fdcc49f9b275f3fa86cbfc395929
hjeong6219 <it.josh.jeong@gmail.com>
2023-10-23 01:07:59 -0400

    added skeleton for the todos

--- frontend/src/app/test/page.jsx
+++ frontend/src/app/test/page.jsx
@@ -1,5 +1,13 @@
+import Loader from "../../components/Loader";
+import TodoSkeleton from "@/components/todo/TodoSkeleton";
+
 function Page() {
-  return <div>Test</div>;
+  return (
+    <div>
+      <Loader />
+      <TodoSkeleton />
+    </div>
+  );
 }
 
 export default Page;
