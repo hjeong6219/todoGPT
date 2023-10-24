53503cac1ec7ce587c4a0d5267b364c910f54f74
hjeong6219 <it.josh.jeong@gmail.com>
2023-10-23 21:34:08 -0400

    added skeleton for the todos

---
+++ frontend/src/components/todo/TodoSkeleton.jsx
@@ -0,0 +1,65 @@
+function TodoSkeleton() {
+  return (
+    <div className="grid grid-cols-1 gap-5 p-4 my-4 bg-gray-100 rounded shadow-lg md:p-6 max-w-screen-2xl md:grid-cols-3">
+      <div className="animate-pulse flex space-x-4">
+        <div className="flex-1 space-y-6 py-1">
+          <div className="flex items-center gap-4 justify-center ">
+            <div className="h-14 flex-1 bg-slate-700 rounded-xl" />
+            <div className="h-14 w-14  bg-slate-700 rounded-xl" />
+            <div className="h-14 w-14  bg-slate-700 rounded-xl" />
+          </div>
+          <div className="flex items-center gap-4 justify-center ">
+            <div className="h-14 flex-1 bg-slate-700 rounded-xl" />
+            <div className="h-14 w-14  bg-slate-700 rounded-xl" />
+            <div className="h-14 w-14  bg-slate-700 rounded-xl" />
+          </div>
+          <div className="flex items-center gap-4 justify-center ">
+            <div className="h-14 flex-1 bg-slate-700 rounded-xl" />
+            <div className="h-14 w-14  bg-slate-700 rounded-xl" />
+            <div className="h-14 w-14  bg-slate-700 rounded-xl" />
+          </div>
+        </div>
+      </div>
+      <div className="animate-pulse flex space-x-4">
+        <div className="flex-1 space-y-6 py-1">
+          <div className="flex items-center gap-4 justify-center ">
+            <div className="h-14 flex-1 bg-slate-700 rounded-xl" />
+            <div className="h-14 w-14  bg-slate-700 rounded-xl" />
+            <div className="h-14 w-14  bg-slate-700 rounded-xl" />
+          </div>
+          <div className="flex items-center gap-4 justify-center ">
+            <div className="h-14 flex-1 bg-slate-700 rounded-xl" />
+            <div className="h-14 w-14  bg-slate-700 rounded-xl" />
+            <div className="h-14 w-14  bg-slate-700 rounded-xl" />
+          </div>
+          <div className="flex items-center gap-4 justify-center ">
+            <div className="h-14 flex-1 bg-slate-700 rounded-xl" />
+            <div className="h-14 w-14  bg-slate-700 rounded-xl" />
+            <div className="h-14 w-14  bg-slate-700 rounded-xl" />
+          </div>
+        </div>
+      </div>
+      <div className="animate-pulse flex space-x-4">
+        <div className="flex-1 space-y-6 py-1">
+          <div className="flex items-center gap-4 justify-center ">
+            <div className="h-14 flex-1 bg-slate-700 rounded-xl" />
+            <div className="h-14 w-14  bg-slate-700 rounded-xl" />
+            <div className="h-14 w-14  bg-slate-700 rounded-xl" />
+          </div>
+          <div className="flex items-center gap-4 justify-center ">
+            <div className="h-14 flex-1 bg-slate-700 rounded-xl" />
+            <div className="h-14 w-14  bg-slate-700 rounded-xl" />
+            <div className="h-14 w-14  bg-slate-700 rounded-xl" />
+          </div>
+          <div className="flex items-center gap-4 justify-center ">
+            <div className="h-14 flex-1 bg-slate-700 rounded-xl" />
+            <div className="h-14 w-14  bg-slate-700 rounded-xl" />
+            <div className="h-14 w-14  bg-slate-700 rounded-xl" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TodoSkeleton;
