68b00e132cf4a0ad012484ae361b83357f0f3e90
hjeong6219 <it.josh.jeong@gmail.com>
2023-10-24 20:28:49 -0400

    modified todoSlice to replace the useState hook for currentTodo

--- frontend/src/app/features/todo/todoSlice.jsx
+++ frontend/src/app/features/todo/todoSlice.jsx
@@ -2,59 +2,23 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const todoSlice = createSlice({
   name: "todos",
-  initialState: [
-    {
-      id: null,
-      title: "title",
-      content: "",
-      completed: false,
-      // dueDate: null,
-      // category: [],
-      // priority: "low",
-      createdAt: "",
-    },
-  ],
+  initialState: { currentTodo: null },
   reducers: {
-    addTodo: (state, action) => {
-      state.todos.push(action.payload);
+    setCurrentTodo: (state, action) => {
+      state.currentTodo = action.payload;
     },
-    updateTodo: (state, action) => {
-      const todos = state.find((todo) => todo.id === action.payload.id);
-      if (todo) {
-        todo = [...todos, action.payload];
-      }
+    updateCurrentTodo: (state, action) => {
+      state.currentTodo = {
+        ...state.currentTodo,
+        ...action.payload,
+      };
     },
-    updateContent: (state, action) => {
-      const todo = state.find((todo) => todo.id === action.payload.id);
-      if (todo) {
-        todo.content = action.payload.content;
-      }
-    },
-    updateTitle: (state, action) => {
-      const todo = state.find((todo) => todo.id === action.payload.id);
-      if (todo) {
-        todo.title = action.payload.title;
-      }
-    },
-    toggleTodo: (state, action) => {
-      const todo = state.find((todo) => todo.id === action.payload);
-      if (todo) {
-        todo.completed = !todo.completed;
-      }
-    },
-    deleteTodo: (state, action) => {
-      return state.filter((todo) => todo.id !== action.payload.id);
+    clearCurrentTodo: (state) => {
+      state.currentTodo = null;
     },
   },
 });
 
-export const {
-  addTodo,
-  updateTodo,
-  toggleTodo,
-  deleteTodo,
-  updateContent,
-  updateTitle,
-} = todoSlice.actions;
+export const { setCurrentTodo, clearCurrentTodo } = todoSlice.actions;
 
 export default todoSlice.reducer;
