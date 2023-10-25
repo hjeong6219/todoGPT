77142703a9fc219523175437a7746a6b91665228
hjeong6219 <it.josh.jeong@gmail.com>
2023-10-24 20:29:14 -0400

    modified todoSlice to replace the useState hook for currentTodo

--- frontend/src/app/store.jsx
+++ frontend/src/app/store.jsx
@@ -3,12 +3,14 @@ import { todosApi } from "./features/todo/todosApi";
 import { usersApi } from "./features/todo/usersApi";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { chatApi } from "./features/chat/chatApi";
+import todoReducer from "./features/todo/todoSlice";
 
 const store = configureStore({
   reducer: {
     [todosApi.reducerPath]: todosApi.reducer,
     [usersApi.reducerPath]: usersApi.reducer,
     [chatApi.reducerPath]: chatApi.reducer,
+    todoSlice: todoReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(
