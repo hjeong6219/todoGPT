9486c5de6e6ccd3740447386620e0901b73d3ca2
hjeong6219 <it.josh.jeong@gmail.com>
2023-10-25 00:22:39 -0400

    renamed the todos page to dashboard

---
+++ frontend/src/app/dashboard/page.jsx
@@ -0,0 +1,19 @@
+import Header from "@/components/Header";
+import TodoList from "../../components/todo/TodoList";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
+import Dashboard from "@/components/dashboard/Dashboard";
+
+function Page() {
+  const { getUser, isAuthenticated } = getKindeServerSession();
+  const user = getUser();
+  return isAuthenticated() ? (
+    <main>
+      <Dashboard user={user} />
+    </main>
+  ) : (
+    redirect("/sign-in")
+  );
+}
+
+export default Page;
