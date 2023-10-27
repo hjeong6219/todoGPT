cc9e996631d8f004c145679de739c349cdd727e9
hjeong6219 <it.josh.jeong@gmail.com>
2023-10-26 20:29:13 -0400

    fixed linkes to navbar

--- frontend/src/components/dashboard/Navbar.jsx
+++ frontend/src/components/dashboard/Navbar.jsx
@@ -1,3 +1,6 @@
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/server";
+import Link from "next/link";
+
 function Navbar() {
   return (
     <nav className="h-full bg-white shadow-lg">
@@ -8,45 +11,42 @@ function Navbar() {
         </div>
         <ul className="space-y-3">
           <li>
-            <a
-              href="#"
+            <Link
+              href="/"
               className="text-gray-700 block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-50 hover:text-blue-600"
             >
               Home
-            </a>
+            </Link>
           </li>
           <li>
-            <a
-              href="#"
+            <Link
+              href="/dashboard"
               className="text-gray-700 block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-50 hover:text-blue-600"
             >
               My Todos
-            </a>
+            </Link>
           </li>
           <li>
-            <a
-              href="#"
+            <Link
+              href="/calendar"
               className="text-gray-700 block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-50 hover:text-blue-600"
             >
               Calendar
-            </a>
+            </Link>
           </li>
           <li>
-            <a
+            <Link
               href="#"
               className="text-gray-700 block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-50 hover:text-blue-600"
             >
               Account
-            </a>
+            </Link>
           </li>
         </ul>
         <div className="mt-6">
-          <a
-            href="#"
-            className="block w-full px-4 py-2 text-center text-gray-700 transition duration-200 border border-gray-300 rounded hover:bg-gray-100"
-          >
+          <LogoutLink className="block w-full px-4 py-2 text-center text-gray-700 transition duration-200 border border-gray-300 rounded hover:bg-gray-100">
             Logout
-          </a>
+          </LogoutLink>
         </div>
       </div>
     </nav>
