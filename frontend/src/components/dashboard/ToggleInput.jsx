2bd08f19ed62edd789a1310da9d012609e183556
hjeong6219 <it.josh.jeong@gmail.com>
2023-10-29 16:17:17 -0400

    cleaned up styling as well as make it respond to user clicking outside of the component

--- frontend/src/components/dashboard/ToggleInput.jsx
+++ frontend/src/components/dashboard/ToggleInput.jsx
@@ -1,20 +1,37 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { HiPlus } from "react-icons/hi";
+import { HiMinus } from "react-icons/hi2";
 
 const ToggleInput = ({ onAddTodo, columnId }) => {
   const [showTextArea, setShowTextArea] = useState(false);
   const [inputValue, setInputValue] = useState("");
+  const containerRef = useRef(null);
 
-  const toggleTextArea = () => {
-    setShowTextArea(!showTextArea);
-  };
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (
+        showTextArea &&
+        containerRef.current &&
+        !containerRef.current.contains(event.target)
+      ) {
+        setInputValue("");
+        setShowTextArea(false);
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [showTextArea]);
 
   const handleKeyPress = (event) => {
     if (event.key === "Enter") {
       event.preventDefault();
-
       onAddTodo(columnId, inputValue);
-
+      setInputValue("");
+      setShowTextArea(false);
+    }
+    if (event.key === "Escape") {
       setInputValue("");
       setShowTextArea(false);
     }
@@ -23,24 +40,28 @@ const ToggleInput = ({ onAddTodo, columnId }) => {
   return (
     <>
       {showTextArea ? (
-        <textarea
-          autoFocus
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-          onKeyPress={handleKeyPress}
-          className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded-lg focus:outline-none focus:shadow-outline"
-          rows="3"
-          placeholder="Enter new todo..."
-        />
+        <div
+          className="flex border-t-2 border-gray-200 hover:border-gray-400 focus:outline-none"
+          ref={containerRef}
+        >
+          <HiMinus className="mt-2 text-xl" />
+          <textarea
+            autoFocus
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            onKeyDown={handleKeyPress}
+            className="mt-2 text-gray-600 rounded-lg resize-none focus:outline-none focus:shadow-outline"
+            rows="1"
+            placeholder="Enter new todo..."
+          />
+        </div>
       ) : (
         <div
-          className="flex border-t-2 border-gray-200 hover:border-gray-400 focus:outline-none cursor-pointer"
-          onClick={toggleTextArea}
+          className="flex border-t-2 border-gray-200 cursor-pointer hover:border-gray-400 focus:outline-none"
+          onClick={() => setShowTextArea(true)}
         >
-          <HiPlus />
-          <button className="ml-2 text-gray-600 focus:outline-none">
-            Add New Todo
-          </button>
+          <HiPlus className="mt-2 text-xl" />
+          <span className="mt-2 text-gray-600">Add New Todo</span>
         </div>
       )}
     </>
