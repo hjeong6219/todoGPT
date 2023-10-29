ade0140f7d61a908585ed6107b4374e335a3dadd
hjeong6219 <it.josh.jeong@gmail.com>
2023-10-29 16:18:21 -0400

    updated minor styling to the alert

--- frontend/src/components/dashboard/Alert.jsx
+++ frontend/src/components/dashboard/Alert.jsx
@@ -1,6 +1,6 @@
 function Alert() {
   return (
-    <div className="absolute left-0 right-0 w-24 p-4 mx-auto my-8 text-red-700 bg-red-200 rounded">
+    <div className="absolute left-0 right-0 p-2 mx-auto my-6 text-red-700 bg-red-200 rounded w-14">
       <p>Alert</p>
     </div>
   );
