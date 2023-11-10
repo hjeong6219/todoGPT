d1f99e398a0e778f11eb744a4cbfc778cfbbacf6
hjeong6219 <it.josh.jeong@gmail.com>
2023-11-10 13:23:23 -0500

    added styling and functionality to the todo menus

--- frontend/src/components/todo/EditorMenu.jsx
+++ frontend/src/components/todo/EditorMenu.jsx
@@ -1,35 +1,114 @@
-function EditorMenu() {
+import { updateCurrentTodo } from "@/app/features/todo/todoSlice";
+import { useUpdateTodoMutation } from "@/app/features/todo/todosApi";
+import dayjs from "dayjs";
+import { useState } from "react";
+import { HiOutlineCalendarDays } from "react-icons/hi2";
+import { useDispatch } from "react-redux";
+
+function EditorMenu({ isMenuOpen, todo }) {
+  const [updateTodo] = useUpdateTodoMutation();
+  const dispatch = useDispatch();
+
+  const handleUpdateTodo = async (option, value) => {
+    const timestamp = dayjs().valueOf();
+    const { data, error } = await updateTodo({
+      _id: todo._id,
+      [option]: value,
+      updatedAt: timestamp,
+    });
+    console.log(value);
+    dispatch(
+      updateCurrentTodo({
+        _id: todo._id,
+        [option]: value,
+        updatedAt: timestamp,
+      })
+    );
+
+    if (data) {
+      console.log("data", data);
+    }
+    if (error) {
+      console.log("error", error);
+    }
+  };
+
   return (
-    <div className="flex flex-col space-y-4">
-      <div className="flex items-center space-x-2">
+    <div
+      className={`z-50 relative right-0 p-4 w-2/5 2xl:w-1/4 h-full overflow-y-hidden rounded-br-xl shadow-lg no-scrollbar bg-stone-50 focus:outline-nonetransform transition-transform  ${
+        isMenuOpen ? "translate-x-0" : "translate-x-full"
+      }`}
+    >
+      <div className="grid grid-cols-1 gap-2 px-4 py-2 border-b-2 border-gray-400 lg:gap-4 lg:py-4 md:text-lg lg:grid-cols-2 ">
         <label className="font-bold">Category:</label>
-        <select className="border border-gray-300 rounded-md">
-          <option value="work">Work</option>
-          <option value="personal">Personal</option>
-          {/* Add more categories as needed */}
+        <select className="w-32 border-none rounded-md md:w-36 bg-stone-50 hover:bg-stone-200 focus:outline-none">
+          <option className="bg-stone-50" value="work">
+            Work
+          </option>
+          <option className="bg-stone-50" value="personal">
+            Personal
+          </option>
         </select>
-      </div>
-      <div className="flex items-center space-x-2">
         <label className="font-bold">Priority:</label>
-        <select className="border border-gray-300 rounded-md">
-          <option value="high">High</option>
-          <option value="medium">Medium</option>
-          <option value="low">Low</option>
+        <select
+          className="w-32 border-none rounded-md md:w-36 bg-stone-50 hover:bg-stone-200 focus:outline-none"
+          value={todo.priority}
+          onChange={(e) => handleUpdateTodo("priority", e.target.value)}
+        >
+          <option className="bg-stone-50" value="">
+            None
+          </option>
+          <option className="bg-stone-50" value="high">
+            High
+          </option>
+          <option className="bg-stone-50" value="medium">
+            Medium
+          </option>
+          <option className="bg-stone-50" value="low">
+            Low
+          </option>
         </select>
-      </div>
-      <div className="flex items-center space-x-2">
         <label className="font-bold">Status:</label>
-        <select className="border border-gray-300 rounded-md">
-          <option value="not-started">Not Started</option>
-          <option value="in-progress">In Progress</option>
-          <option value="completed">Completed</option>
+        <select
+          value={todo.status}
+          onChange={(e) => handleUpdateTodo("status", e.target.value)}
+          className="w-32 border-none rounded-md md:w-36 bg-stone-50 hover:bg-stone-200 focus:outline-none"
+        >
+          <option className="bg-stone-50" value="notStarted">
+            Not Started
+          </option>
+          <option className="bg-stone-50" value="inProgress">
+            In Progress
+          </option>
+          <option className="bg-stone-50" value="completed">
+            Completed
+          </option>
         </select>
+        <label htmlFor="date-picker" className="font-bold text-gray-700">
+          Select a date:
+        </label>
+        <input
+          type="date"
+          id="date-picker"
+          className="left-0 w-32 text-gray-700 border-none rounded-md md:w-36 text-md hover:bg-stone-200 bg-stone-50 form-input focus:outline-none focus:ring-2 focus:ring-indigo-500"
+          value={
+            todo.dueDate ? dayjs(todo.dueDate).toISOString().split("T")[0] : ""
+          }
+          onChange={(e) => {
+            handleUpdateTodo("dueDate", new dayjs(e.target.value).valueOf());
+          }}
+        />
       </div>
-      <div className="flex items-center space-x-2">
-        <label className="font-bold">Due Date:</label>
-        <input type="date" className="border border-gray-300 rounded-md" />
+      <div className="flex-col items-center pt-2 text-sm lg:pt-4 md:text-md xl:flex-row">
+        <div className="font-bold text-gray-400">
+          Created{" "}
+          <span>{dayjs(todo.createdAt).format("MM-DD-YYYY HH:mm:ss")}</span>
+        </div>
+        <div className="font-bold text-gray-400">
+          Last Updated{" "}
+          <span>{dayjs(todo.updatedAt).format("MM-DD-YYYY HH:mm:ss")}</span>
+        </div>
       </div>
-      {/* Add more items as needed */}
     </div>
   );
 }
--- frontend/src/components/todo/TodoContent.jsx
+++ frontend/src/components/todo/TodoContent.jsx
@@ -1,13 +1,15 @@
 "use client";
 import { useRef, useState } from "react";
 import RichTextEditor from "../RichTextEditor";
+import { Editor } from "@tiptap/react";
+import EditorMenu from "./EditorMenu";
 
 function TodoContent({ todo, isMenuOpen }) {
   return (
     <div className="relative flex items-stretch w-full h-full pt-2 mb-12 overflow-y-auto">
       <div
         className={`z-40 rounded-t-sm shadow-lg pb-16  bg-stone-50 focus:outline-none flex-grow  ${
-          isMenuOpen ? "w-4/5 rounded-bl-xl" : "w-full rounded-b-xl"
+          isMenuOpen ? "w-3/5 2xl:w-3/4 rounded-bl-xl" : "w-full rounded-b-xl"
         }`}
       >
         <RichTextEditor
@@ -17,13 +19,7 @@ function TodoContent({ todo, isMenuOpen }) {
           // setContent={setContent}
         />
       </div>
-      {isMenuOpen && (
-        <div
-          className={`z-50 relative right-0  w-1/5 h-full overflow-y-hidden rounded-br-xl shadow-lg no-scrollbar bg-stone-50 focus:outline-nonetransform transition-transform  ${
-            isMenuOpen ? "translate-x-0" : "translate-x-full"
-          }`}
-        ></div>
-      )}
+      {isMenuOpen && <EditorMenu isMenuOpen={isMenuOpen} todo={todo} />}
       {/* <Chat todoId={currentTodo._id} /> */}
     </div>
   );
