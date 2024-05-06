1accbbb51c861bf962630a2d9bd9767b933457f9
hjeong6219 <it.josh.jeong@gmail.com>
2024-05-06 10:16:10 -0400

    Fixed routes for the backend api

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,9 @@ require("dotenv").config();
 const connectDB = require("../../db");
 const express = require("express");
 const app = express();
-const todos = require("./todos");
-const users = require("./users");
-const chats = require("./chats");
+const todos = require("./routes/api/todos");
+const users = require("./routes/api/users");
+const chats = require("./routes/api/chats");
 const cors = require("cors");
 
 connectDB();
@@ -22,6 +22,10 @@ app.use(express.json());
 app.use("/todos", todos);
 app.use("/users", users);
 app.use("/chats", chats);
+
+app.use("/", (req, res) => {
+  res.send("Welcome to the TodoGPT API");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
